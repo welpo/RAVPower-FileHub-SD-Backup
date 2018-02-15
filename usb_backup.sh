9c5bacc9a9a0aab70763f55145e6b438d631dfc1
Frank Neulichedl <frank.neulichedl@kaufland.com>
2018-02-15 14:38:34 +0100

    Pull request

--- usb_backup.sh
+++ usb_backup.sh
@@ -31,7 +31,7 @@ STORE_DIR=/sdcopies
 BACKUP_DIR=/fotobackup
 PHOTO_DIR="$STORE_DIR"/fotos
 CONFIG_DIR="$STORE_DIR"/config
-MEDIA_REGEX=".*\.\(jpg\|gif\|png\|jpeg\|mov\|avi\|wav\|mp3\|aif\|wma\|wmv\|asx\|asf\|m4v\|mp4\|mpg\|3gp\|3g2\|crw\|cr2\|nef\|dng\|mdc\|orf\|sr2\|srf\|mts\)"
+#MEDIA_REGEX=".*\.\(jpg\|gif\|png\|jpeg\|mov\|avi\|wav\|mp3\|aif\|wma\|wmv\|asx\|asf\|m4v\|mp4\|mpg\|3gp\|3g2\|crw\|cr2\|nef\|dng\|mdc\|orf\|sr2\|srf\|mts\|rw2\)"
 
 # Check if an SD card is inserted (always mounted at the same mount point on the Rav Filehub)
 check_sdcard() {
@@ -99,13 +99,19 @@ backupdrive=$?
 if [ $sdcard -eq 1 -a $storedrive -eq 1 ];then
         # Organize the photos in a folder for each SD card by UUID,
         target_dir="$store_mountpoint$PHOTO_DIR"/"$sd_uuid"
+	log_dir="$store_mountpoint$STORE_DIR"/log
         mkdir -p $target_dir
-		mkdir -p "$STORE_DIR"/log
+        mkdir -p $log_dir 
         # Copy the files from the sd card to the target dir, 
         # Uses filename and size to check for duplicates
-        echo "$(date): Copying SD card to $target_dir" >> "$STORE_DIR"/log/usb_add_info
-        rsync -vrmt -v --size-only --log-file "$store_mountpoint$PHOTO_DIR"/tmp/rsync_log --exclude ".?*" "$SD_MOUNTPOINT"/DCIM/ "$target_dir"/DCIM/
-        rsync -vrmt -v --size-only --log-file "$store_mountpoint$PHOTO_DIR"/tmp/rsync_log --exclude ".?*" "$SD_MOUNTPOINT"/PRIVATE/ "$target_dir"/PRIVATE/
+        echo "$(date): Copying SD card $SD_MOUNTPOINT to $target_dir" >> "$log_dir"/usb_add_info
+        rsync -vrm --size-only --log-file $log_dir/rsync_log --exclude ".?*" \
+                $SD_MOUNTPOINT/DCIM \
+                $SD_MOUNTPOINT/PRIVATE \
+                $SD_MOUNTPOINT/MISC \
+                $SD_MOUNTPOINT/MP_ROOT \
+                $SD_MOUNTPOINT/AVF_INFO \
+                $target_dir >> "$log_dir"/rsync_stdout
 fi
 
 # If both a valid store drive and a matching backup drive are attached,
@@ -114,12 +120,13 @@ if [ $storedrive -eq 1 -a $backupdrive -eq 1 -a "$backup_id" == "$store_id" ]; t
         source_dir="$store_mountpoint$STORE_DIR"
         target_dir="$backup_mountpoint$BACKUP_DIR"
         partial_dir="$store_mountpoint$PHOTO_DIR"/incoming/.partial
-        echo "Backing up data store to $target_dir" >> "$STORE_DIR"/log/usb_add_info
-        rsync -vrmt --size-only --delete-during --exclude ".?*" --partial-dir "$partial_dir" --exclude "swapfile" --log-file "$store_mountpoint$PHOTO_DIR"/tmp/rsync_log "$source_dir"/ "$target_dir"
+		log_dir="$store_mountpoint"/log
+        echo "Backing up data store to $target_dir" >> "$log_dir"/usb_add_info
+        rsync -vrm --size-only --delete-during --exclude ".?*" --partial-dir "$partial_dir" --exclude "swapfile" --log-file "$log_dir"/rsync_log "$source_dir"/ "$target_dir"
         if  [ $? -eq 0 ]; then
-                echo "$(date): Backup complete" >> "$STORE_DIR"/log/usb_add_info
+                echo "$(date): Backup complete" >> "$log_dir"/usb_add_info
         else
-                echo "$(date): Backup failed" >> "$STORE_DIR"/log/usb_add_info
+                echo "$(date): Backup failed" >> "$log_dir"/usb_add_info
         fi
 fi
 
